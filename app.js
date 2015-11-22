afe19e7955d0eee6b3de4e3c97e71408c38508f1
sallareznov <diagne14@gmail.com>
2015-11-22 19:10:43 +0100

    :heavy_plus_sign: Glue of the application

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 var express = require('express');
-var app = express();
+var morgan = require('morgan');
+var favicon = require('serve-favicon');
+var cookieParser = require('cookie-parser');
+var logger = require('./helper/logger');
+var path = require('path');
 var model = require('./model/model');
 var index = require('./controller/index');
-var username = process.argv[2];
 
+var app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-var server = app.listen(8000, function () {
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = app.listen(8000, '0.0.0.0', function () {
   var host = server.address().address;
   var port = server.address().port;
+  var watchedDirectory = process.argv[3];
 
   console.log('Example app listening at http://%s:%s', host, port);
+  model.setWatchedDirectory(watchedDirectory);
 });
 
 app.use('/', index);
